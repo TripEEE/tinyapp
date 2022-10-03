faf182c95599d1e453907fb8b5879a9085bbbfda
TripEEE <evan.trippel@gmail.com>
2022-10-03 16:09:58 -0400

    Added HTML and for loop to urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -2,18 +2,22 @@ const express = require("express")
 const app = express()
 const PORT = 8080
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase }; //urlDatabase becomes urls in urls_index.js
+  res.render("urls_index", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!")
 })
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
-})
 
 app.get("/urls.json"), (req, res) => {
   res.json(urlDatabase)
@@ -22,3 +26,8 @@ app.get("/urls.json"), (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello<b>World</b></body></html>\n")
 })
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
+})
+
